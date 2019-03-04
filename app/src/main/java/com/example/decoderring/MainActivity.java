fce32b57b2fb6ea0a74c6217c970bfd1bd49b8e0
AdamcBourque <adamobourque@gmail.com>
2019-03-04 16:34:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/decoderring/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.decoderring;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.InputMismatchException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buildTable(View view) {
+        int padding = (int)(getResources().getDimension(R.dimen.passing)/getResources().getDisplayMetrics().density);
+        float tablefont = getResources().getDimension(R.dimen.table_text);
+        CheckBox square = findViewById(R.id.square);
+        CheckBox cube = findViewById(R.id.cube);
+        CheckBox sqrt = findViewById(R.id.sqrt);
+        CheckBox fact = findViewById(R.id.factorial);
+        EditText min = findViewById(R.id.min);
+        EditText max = findViewById(R.id.max);
+        int minVal, maxVal;
+        try {
+            minVal = Integer.parseInt(min.getText().toString());
+            maxVal = Integer.parseInt(max.getText().toString());
+        } catch (InputMismatchException e) {
+            minVal = 0;
+            maxVal =0;
+        }
+        TableLayout table = findViewById(R.id.table);
+
+        for (int i = minVal; i<=maxVal; i++){
+            TableRow newRow = new TableRow(this);
+            TextView n = new TextView(this);
+            setTextView(n, "" + i ,padding);
+            TextView n2 = new TextView(this);
+            setTextView(n2, "" + (i*i) ,padding);
+            TextView n3 = new TextView(this);
+            setTextView(n3, "" + (i*i*i) ,padding);
+            TextView _2n = new TextView(this);
+            setTextView(_2n, "" + (i^(1/2)) ,padding);
+            TextView nfact = new TextView(this);
+            int temp = 1;
+            for (int j = 1; j <= i; j++){
+                temp*=j;
+            }
+            setTextView(nfact, "" + temp ,padding);
+            table.addView(newRow);
+            newRow.addView(n);
+            if (square.isChecked() == true){
+                newRow.addView(n2);
+            }
+            if (cube.isChecked() == true){
+                newRow.addView(n3);
+            }
+            if (sqrt.isChecked() == true){
+                newRow.addView(_2n);
+            }
+            if (fact.isChecked() == true){
+                newRow.addView(nfact);
+            }
+        }
+
+
+
+    }
+    public void setTextView(TextView t, String s, int pad){
+        t.setText(s);
+        t.setTextSize(12);
+        t.setPadding(0,0, pad,0);
+    }
+}
